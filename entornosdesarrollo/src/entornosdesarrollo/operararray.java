409915eb55b0359120e57ec17fac447359200260
diurno <diurno@pc-aula>
2019-03-11 08:50:44 +0100

    hola

---
+++ entornosdesarrollo/src/entornosdesarrollo/operararray.java
@@ -0,0 +1,20 @@
+package entornosdesarrollo;
+
+public class operararray {
+	static int array[] = new int [10];
+	static int numAleatorio = 0;
+	
+
+	public static void main(String[] args) {
+		array();
+		
+	}
+	public static void array () {
+		for (int i = 0; i < array.length; i++) {
+			numAleatorio = (int)Math.round(Math.random()*(20-1)+1);
+			array[i] = numAleatorio;
+			
+		}
+	}
+
+}
